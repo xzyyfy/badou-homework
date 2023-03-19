92faf497cb8e15bb9ddb8b5228baabf8afcf2e58
ddikk <36041728+ddikk@users.noreply.github.com>
2023-03-19 09:08:38 +0800

    交作业

---
+++ 78-曹文钰/lesson6.py
@@ -0,0 +1,57 @@
+import json
+import copy
+from ngram_language_model import NgramLanguageModel
+"""
+文本纠错demo
+加载同音字字典
+加载语言模型
+基本原理：
+对于文本中每一个字，判断在其同音字中是否有其他字，在替换掉该字时，能使得语言模型计算的成句概率提高
+"""
+
+
+class Corrector:
+    def __init__(self, language_model):
+        #语言模型
+        self.language_model = language_model
+        #候选字字典
+        self.sub_dict = self.load_tongyinzi("tongyin.txt")
+        #成句概率的提升超过阈值则保留修改
+        self.threshold = 7
+
+    #实际上不光是同音字，同形字等也可以加入，本质上是常用的错字
+    def load_tongyinzi(self, path):
+        tongyinzi_dict = {}
+        with open(path, encoding="utf8") as f:
+            for line in f:
+                char, tongyin_chars = line.split()
+                tongyinzi_dict[char] = list(tongyin_chars)
+        return tongyinzi_dict
+
+    #纠错逻辑
+    def correction(self, string):
+        #todo  自己尝试实现
+        to_replace = {}
+        before = self.language_model.predict(string)
+        for i, word in enumerate(string):
+            similar_pronounce = self.sub_dict.get(word, [])
+            if not similar_pronounce:
+                continue
+            for each in similar_pronounce:
+                after = self.language_model.predict(string[:i]+each+string[i+1:])
+                if after - before >= self.threshold:
+                    to_replace[i] = each
+                    break
+        for i, each in to_replace.items():
+            string = string[:i] + each + string[i + 1:]
+        return string
+
+
+corpus = open("财经.txt", encoding="utf8").readlines()
+lm = NgramLanguageModel(corpus, 3)
+
+cr = Corrector(lm)
+string = "每国货币政册空间不大"  #美国货币政策空间不大
+fix_string = cr.correction(string)
+print("修改前：", string)
+print("修改后：", fix_string)
